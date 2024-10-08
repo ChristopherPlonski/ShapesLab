e5152605564a2225a4331876dd4546e048fc50e4
NNR8608220 <n8reyes2006@gmail.com>
2024-10-08 15:37:18 -0500

    Created Square Class

---
+++ test_Square.cpp
@@ -0,0 +1,24 @@
+#include "Square.hpp"
+#include "Rectangle.cpp"
+#include <iostream>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+    Square square = Square(5.00);
+
+    assert(square.getArea() == 25.00);
+    assert(square.getPerimeter() == 20.00);
+
+    square = Square(3.00);
+
+    assert(square.getArea() == 9.00);
+    assert(square.getPerimeter() == 12.00);
+
+    cout << "Code Works" << endl;
+
+
+    return 0;
+}
