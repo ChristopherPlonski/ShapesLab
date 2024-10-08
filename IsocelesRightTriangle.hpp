9555f5fb7f8ed7e6c7d49c5a2bb0a0f8512efae7
Christopher Plonski <christopherplonski@gmail.com>
2024-10-08 15:54:08 -0500

    IsocelesRightTriangle Class

---
+++ IsocelesRightTriangle.hpp
@@ -0,0 +1,11 @@
+#ifndef ISOCELESRIGHTTRIANGLE_H
+#define ISOCELESRIGHTTRIANGLE_H
+
+#include "RightTriangle.hpp"
+
+class IsocelesRightTriangle : public RightTriangle {
+public:
+	IsocelesRightTriangle(double legLength, double hypotenusLength);
+};
+
+#endif // !ISOCELESRIGHTTRIANGLE_H
